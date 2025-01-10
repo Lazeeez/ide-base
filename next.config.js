2bbc94fc1f95332ab5768fac717280c38137879a
lazeez <lajatm9@gmail.com>
2025-01-10 21:20:41 +0900

    fixed missing private key

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const withConfig = withGlobalCss();
 module.exports = withConfig({
   env: {
     IS_TEST_ENV: process.env.IS_TEST_ENV,
+    NEW_NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEW_NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEW_FIREBASE_PRIVATE_KEY: process.env.NEW_FIREBASE_PRIVATE_KEY,
+    NEW_FIREBASE_CLIENT_EMAIL: process.env.NEW_FIREBASE_CLIENT_EMAIL,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
